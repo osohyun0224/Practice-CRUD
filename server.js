cad99015a2c73f75bfc0e6c41c2d2c43d529e2f5
Garden <garden@bzznbyd.com>
2024-01-24 17:11:32 +0900

    📦 chores: nodemon 설치

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('hello node api')
 })
 
+app.get('/blog', (req, res) => {
+    res.send('hello node api')
+})
+
 
 app.listen(3000, ()=> {
     console.log(`Node API app is running on port 3000`)
