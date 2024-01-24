0e146341d36f1f07fda33720e7a2b2b383b67f80
Garden <garden@bzznbyd.com>
2024-01-24 16:33:59 +0900

    ✨ feat: 포트 3000에 엑세스 하기 위한 라우트 설정

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
+//routes
+app.get('/', (req, res) => {
+    res.send('hello node api')
+})
+
+
 app.listen(3000, ()=> {
     console.log(`Node API app is running on port 3000`)
 })
